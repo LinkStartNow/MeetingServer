c74cdd1f8faff0f91fd4d9b532379b6308c6a4d6
LinkStartNow <1254336479@qq.com>
2024-04-21 10:34:26 +0800

    解决tcp通信稳定性问题

--- mynet/Tcpsock.cpp
+++ mynet/Tcpsock.cpp
@@ -7,41 +7,89 @@ using namespace std;
 
 bool Tcpsock::Write(const char* buf, int size)
 {
-	if (send(m_sock, (char*)&size, sizeof(int), 0) == -1) {
-		perror("send size failed");
-		return false;
-	}
+    char* sz = (char*)&size;
+    int num = 0;
+    int max = sizeof(size);
+    while (num < max) {
+        int t;
+        if ((t = send(m_sock, sz + num, max - num, 0)) == -1) {
+            perror("send size failed");
+            return false;
+        }
+        num += t;
+    }
+
+    num = 0;
+    max = size;
+
+    while (num < max) {
+        int t;
+        if ((t = send(m_sock, buf + num, max - num, 0)) == -1) {
+            perror("send size failed");
+            return false;
+        }
+        num += t;
+    }
 	
-	if (send(m_sock, buf, size, 0) == -1) {
-		perror("send buf failed");
-		return false;
-	}
+//	if (send(m_sock, buf, size, 0) == -1) {
+//		perror("send buf failed");
+//		return false;
+//	}
 
 	return true;
 }
 
 char* Tcpsock::Read()
 {
-	int t;
-    int num;
-    if ((t = recv(m_sock, (char*)&num, sizeof(int), 0)) == -1) {
-		perror("read size failed");
-        return nullptr;
-	}
+//	int t;
+    int num = 0;
+    int max = sizeof(int);
+    char sz[max];
 
-    if (!t) {
-		cout << "发送端已经退出，结束通信" << endl;
-        return nullptr;
-	}
+    while (num < max) {
+        int t;
+        if ((t = recv(m_sock, sz + num, max - num, 0)) == -1) {
+            perror("send size failed");
+            return nullptr;
+        }
+        if (!t) {
+            cout << "发送端已经退出，结束通信" << endl;
+            return nullptr;
+        }
+        num += t;
+    }
+
+//    if ((t = recv(m_sock, (char*)&num, sizeof(int), 0)) == -1) {
+//		perror("read size failed");
+//        return nullptr;
+//	}
+
+//    if (!t) {
+//		cout << "发送端已经退出，结束通信" << endl;
+//        return nullptr;
+//	}
+
+    num = *(int*)(sz);
 
     cout << "num:" << num << endl;
 
     char* buf = new char[num];
+    max = num;
+    num = 0;
+
+    while (num < max) {
+        int t;
+        if ((t = recv(m_sock, buf + num, max - num, 0)) == -1) {
+            perror("send size failed");
+            return nullptr;
+        }
+        num += t;
+    }
 	
-	if (recv(m_sock, buf, num, 0) == -1) {
-		perror("read buf failed");
-        return nullptr;
-	}
+//	if (recv(m_sock, buf, num, 0) == -1) {
+//		perror("read buf failed");
+//        return nullptr;
+//	}
     return buf;
 }
 
